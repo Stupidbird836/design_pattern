8cfd22d42a20b594e015f568f277c3836008a5e2
huchenfei <huchenfei@chunbo.com>
2020-08-14 17:11:17 +0800

    ii

--- src/pattern/template/HummerH1Model.java
+++ src/pattern/template/HummerH1Model.java
@@ -1,23 +1,51 @@
 package pattern.template;
 
-public class HummerH1Model extends HummerModel {
+/**
+ * @author huchenfei
+ */
+public class HummerH1Model extends AbstractHummerModel {
 
+//    private HummerH2Model hummerH2Model;
+//
+//    public HummerH2Model getHummerH2Model() {
+//        return hummerH2Model;
+//    }
+//
+//    public HummerH1Model setHummerH2Model(HummerH2Model hummerH2Model) {
+//        this.hummerH2Model = hummerH2Model;
+//        return this;
+//    }
+
+    public HummerH1Model() {
+        System.out.println("b");
+    }
+//
+//    public HummerH1Model(HummerH2Model hummerH2Model) {
+//        this.hummerH2Model = hummerH2Model;
+//        this.hummerH2Model.alarm();
+//    }
+
+    @Override
     protected void alarm() {
         System.out.println("悍马H1鸣笛...");
     }
 
+    @Override
     protected void engineBoom() {
         System.out.println("悍马H1引擎声音是这样的...");
     }
 
+    @Override
     protected void start() {
         System.out.println("悍马H1发动...");
     }
 
+    @Override
     protected void stop() {
         System.out.println("悍马H1停车...");
     }
 
+    @Override
     protected boolean isAlarm() {
         return true;
     }
--- src/pattern/template/TemplateMethodPattern.java
+++ src/pattern/template/TemplateMethodPattern.java
@@ -1,16 +1,20 @@
 package pattern.template;
 
+import java.util.Calendar;
+import java.util.Date;
+
 /**
  * 模版方法模式
+ * @author huchenfei
  */
 public class TemplateMethodPattern {
 
     public static void main(String[] args) {
-        HummerModel h1 = new HummerH1Model();
-        h1.run();
-
-        HummerModel h2 = new HummerH2Model();
-        h2.run();
+        /*HummerH1Model h1 = new HummerH1Model();
+        h1.run();*/
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        System.out.println(calendar.get(Calendar.MONTH) + 1 + "月" + calendar.get(Calendar.DAY_OF_MONTH) + "日");
     }
 
 }
